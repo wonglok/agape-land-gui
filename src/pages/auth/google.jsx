7c8e5141cee9eee32ec196e23a722e567247a636
Wong Lok <wonglok@users.noreply.github.com>
2022-11-27 21:26:54 +0800

    yi

--- src/pages/auth/google.jsx
+++ src/pages/auth/google.jsx
@@ -4,7 +4,7 @@ import { getID } from '@/lib/getID'
 
 export function GoogleContent() {
   let myEndPoints = {
-    development: `https://82xi9xvuu6.execute-api.ap-southeast-1.amazonaws.com`,
+    development: `https://lspr7w8538.execute-api.ap-southeast-1.amazonaws.com`,
     production: `https://via39ii0sd.execute-api.ap-southeast-1.amazonaws.com`,
     test: `https://xulbvrr82m.execute-api.ap-southeast-1.amazonaws.com`,
   }
