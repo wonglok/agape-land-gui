3081ee3fc97e498e9f5b75d9b73205a7ac3f529e
Wong Lok <wonglok@users.noreply.github.com>
2022-12-11 20:55:03 +0800

    loading

--- src/content-landing-page/LoginContentGate/LoadingGroup.js
+++ src/content-landing-page/LoginContentGate/LoadingGroup.js
@@ -1,18 +1,21 @@
 import { Image } from '@react-three/drei'
+import { Suspense } from 'react'
 import { BackgroundColor } from '../NYCJourney/BackgroundColor'
 
 export function LoadingGroup() {
   return (
     <group>
-      {/*  */}
-      <Image
-        position={[0, 0, 0]}
-        scale={[2.39, 0.61]}
-        transparent={true}
-        url={`/brand/agape-2.png`}
-      ></Image>
-      {/*  */}
-      <BackgroundColor color='#F08BDC'></BackgroundColor>
+      <Suspense fallback={null}>
+        {/*  */}
+        <Image
+          position={[0, 0, 0]}
+          scale={[2.39, 0.61]}
+          transparent={true}
+          url={`/brand/agape-2.png`}
+        ></Image>
+        {/*  */}
+        <BackgroundColor color='#F08BDC'></BackgroundColor>
+      </Suspense>
     </group>
   )
 }
