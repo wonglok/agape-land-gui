51b741a82eab2dfc2b78c50983839e17c60bfe19
Wong Lok <wonglok@users.noreply.github.com>
2022-12-13 09:20:09 +0800

    init

--- src/lib/walker/Game.js
+++ src/lib/walker/Game.js
@@ -7,6 +7,7 @@ import { Line3 } from 'three'
 
 export class Game {
   constructor({ startAt = [0, 3, 0], core = false, name = false, collider }) {
+    console.log('init game:', name)
     //
     if (!core) {
       throw new Error('need a core')
@@ -71,148 +72,150 @@ export class Game {
     this.tempMat = new Matrix4()
     this.tempSegment = new Line3()
 
-    this.updatePlayer = (delta) => {
-      let { camera, controls } = this.core.now
-      let collider = this.collider
-      if (!camera) {
-        return
-      }
-
-      if (!controls) {
-        return
-      }
-
-      this.playerVelocity.y += this.playerIsOnGround ? 0 : delta * this.gravity
-      this.player.position.addScaledVector(this.playerVelocity, delta)
-
-      // move the player
-      const angle = controls.getAzimuthalAngle()
-      if (this.keyState.fwdPressed) {
-        this.tempVector.set(0, 0, -1).applyAxisAngle(this.upVector, angle)
-        this.player.position.addScaledVector(
-          this.tempVector,
-          this.playerSpeed * delta
-        )
-      }
-
-      if (this.keyState.bkdPressed) {
-        this.tempVector.set(0, 0, 1).applyAxisAngle(this.upVector, angle)
-        this.player.position.addScaledVector(
-          this.tempVector,
-          this.playerSpeed * delta
-        )
-      }
-
-      if (this.keyState.lftPressed) {
-        this.tempVector.set(-1, 0, 0).applyAxisAngle(this.upVector, angle)
-        this.player.position.addScaledVector(
-          this.tempVector,
-          this.playerSpeed * delta
-        )
-      }
-
-      if (this.keyState.rgtPressed) {
-        this.tempVector.set(1, 0, 0).applyAxisAngle(this.upVector, angle)
-        this.player.position.addScaledVector(
-          this.tempVector,
-          this.playerSpeed * delta
-        )
-      }
-
-      this.player.updateMatrixWorld()
-
-      // adjust player position based on collisions
-      const capsuleInfo = this.player.capsuleInfo
-      this.tempBox.makeEmpty()
-      this.tempMat.copy(this.collider.matrixWorld).invert()
-      this.tempSegment.copy(capsuleInfo.segment)
-
-      // get the position of the capsule in the local space of the collider
-      this.tempSegment.start
-        .applyMatrix4(this.player.matrixWorld)
-        .applyMatrix4(this.tempMat)
-      this.tempSegment.end
-        .applyMatrix4(this.player.matrixWorld)
-        .applyMatrix4(this.tempMat)
-
-      // get the axis aligned bounding box of the capsule
-      this.tempBox.expandByPoint(this.tempSegment.start)
-      this.tempBox.expandByPoint(this.tempSegment.end)
-
-      this.tempBox.min.addScalar(-capsuleInfo.radius)
-      this.tempBox.max.addScalar(capsuleInfo.radius)
-
-      this.collider.geometry.boundsTree.shapecast({
-        intersectsBounds: (box) => box.intersectsBox(this.tempBox),
-
-        intersectsTriangle: (tri) => {
-          // check if the triangle is intersecting the capsule and adjust the
-          // capsule position if it is.
-          const triPoint = this.tempVector
-          const capsulePoint = this.tempVector2
-
-          const distance = tri.closestPointToSegment(
-            this.tempSegment,
-            triPoint,
-            capsulePoint
-          )
-          if (distance < capsuleInfo.radius) {
-            const depth = capsuleInfo.radius - distance
-            const direction = capsulePoint.sub(triPoint).normalize()
-
-            this.tempSegment.start.addScaledVector(direction, depth)
-            this.tempSegment.end.addScaledVector(direction, depth)
-          }
-        },
-      })
-
-      // get the adjusted position of the capsule collider in world space after checking
-      // triangle collisions and moving it. capsuleInfo.segment.start is assumed to be
-      // the origin of the player model.
-      const newPosition = this.tempVector
-      newPosition
-        .copy(this.tempSegment.start)
-        .applyMatrix4(collider.matrixWorld)
-
-      // check how much the collider was moved
-      const deltaVector = this.tempVector2
-      deltaVector.subVectors(newPosition, this.player.position)
-
-      // if the player was primarily adjusted vertically we assume it's on something we should consider ground
-      this.playerIsOnGround =
-        deltaVector.y > Math.abs(delta * this.playerVelocity.y * 0.25)
-
-      const offset = Math.max(0.0, deltaVector.length() - 1e-5)
-      deltaVector.normalize().multiplyScalar(offset)
-
-      // adjust the player model
-      this.player.position.add(deltaVector)
-
-      if (!this.playerIsOnGround) {
-        deltaVector.normalize()
-        this.playerVelocity.addScaledVector(
-          deltaVector,
-          -deltaVector.dot(this.playerVelocity)
-        )
-      } else {
-        this.playerVelocity.set(0, 0, 0)
-      }
-
-      // adjust the camera
-      camera.position.sub(controls.target)
-      controls.target.copy(this.player.position)
-      camera.position.add(this.player.position)
-
-      // if the player has fallen too far below the level reset their position to the start
-      if (this.player.position.y < -25) {
-        this.reset()
-      }
-
-      console.log(this.player.position.toArray())
-    }
     this.keyboardCtrls = new KeyboardControls({ core: this.core, parent: this })
     //
   }
+
+  updatePlayer(delta) {
+    let collider = this.collider
+
+    let { camera, controls } = this.core.now
+
+    if (!camera) {
+      return
+    }
+    if (!controls) {
+      return
+    }
+    if (!collider) {
+      return
+    }
+
+    this.playerVelocity.y += this.playerIsOnGround ? 0 : delta * this.gravity
+    this.player.position.addScaledVector(this.playerVelocity, delta)
+
+    // move the player
+    const angle = controls.getAzimuthalAngle()
+    if (this.keyState.fwdPressed) {
+      this.tempVector.set(0, 0, -1).applyAxisAngle(this.upVector, angle)
+      this.player.position.addScaledVector(
+        this.tempVector,
+        this.playerSpeed * delta
+      )
+    }
+
+    if (this.keyState.bkdPressed) {
+      this.tempVector.set(0, 0, 1).applyAxisAngle(this.upVector, angle)
+      this.player.position.addScaledVector(
+        this.tempVector,
+        this.playerSpeed * delta
+      )
+    }
+
+    if (this.keyState.lftPressed) {
+      this.tempVector.set(-1, 0, 0).applyAxisAngle(this.upVector, angle)
+      this.player.position.addScaledVector(
+        this.tempVector,
+        this.playerSpeed * delta
+      )
+    }
+
+    if (this.keyState.rgtPressed) {
+      this.tempVector.set(1, 0, 0).applyAxisAngle(this.upVector, angle)
+      this.player.position.addScaledVector(
+        this.tempVector,
+        this.playerSpeed * delta
+      )
+    }
+
+    this.player.updateMatrixWorld()
+
+    // adjust player position based on collisions
+    const capsuleInfo = this.player.capsuleInfo
+    this.tempBox.makeEmpty()
+    this.tempMat.copy(this.collider.matrixWorld).invert()
+    this.tempSegment.copy(capsuleInfo.segment)
+
+    // get the position of the capsule in the local space of the collider
+    this.tempSegment.start
+      .applyMatrix4(this.player.matrixWorld)
+      .applyMatrix4(this.tempMat)
+    this.tempSegment.end
+      .applyMatrix4(this.player.matrixWorld)
+      .applyMatrix4(this.tempMat)
+
+    // get the axis aligned bounding box of the capsule
+    this.tempBox.expandByPoint(this.tempSegment.start)
+    this.tempBox.expandByPoint(this.tempSegment.end)
+
+    this.tempBox.min.addScalar(-capsuleInfo.radius)
+    this.tempBox.max.addScalar(capsuleInfo.radius)
+
+    this.collider.geometry.boundsTree.shapecast({
+      intersectsBounds: (box) => box.intersectsBox(this.tempBox),
+
+      intersectsTriangle: (tri) => {
+        // check if the triangle is intersecting the capsule and adjust the
+        // capsule position if it is.
+        const triPoint = this.tempVector
+        const capsulePoint = this.tempVector2
+
+        const distance = tri.closestPointToSegment(
+          this.tempSegment,
+          triPoint,
+          capsulePoint
+        )
+        if (distance < capsuleInfo.radius) {
+          const depth = capsuleInfo.radius - distance
+          const direction = capsulePoint.sub(triPoint).normalize()
+
+          this.tempSegment.start.addScaledVector(direction, depth)
+          this.tempSegment.end.addScaledVector(direction, depth)
+        }
+      },
+    })
+
+    // get the adjusted position of the capsule collider in world space after checking
+    // triangle collisions and moving it. capsuleInfo.segment.start is assumed to be
+    // the origin of the player model.
+    const newPosition = this.tempVector
+    newPosition.copy(this.tempSegment.start).applyMatrix4(collider.matrixWorld)
+
+    // check how much the collider was moved
+    const deltaVector = this.tempVector2
+    deltaVector.subVectors(newPosition, this.player.position)
+
+    // if the player was primarily adjusted vertically we assume it's on something we should consider ground
+    this.playerIsOnGround =
+      deltaVector.y > Math.abs(delta * this.playerVelocity.y * 0.25)
+
+    const offset = Math.max(0.0, deltaVector.length() - 1e-5)
+    deltaVector.normalize().multiplyScalar(offset)
+
+    // adjust the player model
+    this.player.position.add(deltaVector)
+
+    if (!this.playerIsOnGround) {
+      deltaVector.normalize()
+      this.playerVelocity.addScaledVector(
+        deltaVector,
+        -deltaVector.dot(this.playerVelocity)
+      )
+    } else {
+      this.playerVelocity.set(0, 0, 0)
+    }
+
+    // adjust the camera
+    camera.position.sub(controls.target)
+    controls.target.copy(this.player.position)
+    camera.position.add(this.player.position)
+
+    // if the player has fallen too far below the level reset their position to the start
+    if (this.player.position.y < -25) {
+      this.reset()
+    }
+  }
+
   reset() {
     let { camera, controls } = this.core.now
     if (!camera) {
@@ -250,9 +253,7 @@ class KeyboardControls {
           this.parent.keyState.lftPressed = true
           break
         case 'Space':
-          if (this.parent.playerIsOnGround) {
-            this.parent.playerVelocity.y = 10.0
-          }
+          this.parent.playerVelocity.y = 5.0
 
           break
       }
--- src/lib/walker/Walker.jsx
+++ src/lib/walker/Walker.jsx
@@ -9,13 +9,18 @@ export function Walker({ startAt = [0, 3, 0], name, collider }) {
   let core = useCore()
 
   let game = useMemo(() => {
-    let newGame = new Game({ startAt, name, core, collider })
-    WalkerState[name] = newGame
-    game = newGame
+    let game = new Game({ startAt, name, core, collider })
+    WalkerState[name] = game
+
+    return game
   }, [name, startAt, core, collider])
 
   useFrame((st, dt) => {
     if (game) {
+      if (dt >= 1 / 30) {
+        dt = 1 / 30
+      }
+
       game.updatePlayer(dt)
     }
   })
