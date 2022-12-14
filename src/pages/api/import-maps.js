fccd9fcb66f4591a8d5de398c01c1e8e2904cbfa
Wong Lok <wonglok@users.noreply.github.com>
2022-12-14 17:25:41 +0800

    vercel

--- src/pages/api/import-maps.js
+++ src/pages/api/import-maps.js
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server'
+
 import { Generator } from '@jspm/generator'
 
 export default async function handler(req, res) {
@@ -12,7 +14,7 @@ export default async function handler(req, res) {
     await generator.install(bodyData.packages[packID])
   }
 
-  return res.status(200).json(generator.getMap())
+  return NextResponse.json(generator.getMap())
 }
 
 export const config = {
