7b5a4a3d12129c38e9be69857db107fc3cb0e52d
Wong Lok <wonglok@users.noreply.github.com>
2022-12-14 17:45:05 +0800

    vercel

--- src/content-landing-page/LoginContentGate/GateConst.js
+++ src/content-landing-page/LoginContentGate/GateConst.js
@@ -1,7 +1,5 @@
+import { UserEndPoints } from '@/aws/UserEndPoints'
+
 export const SESSION_ACCESS_KEY = `session-access-key`
 
-export const UserEndPoints = {
-  development: `https://lspr7w8538.execute-api.ap-southeast-1.amazonaws.com`,
-  production: `https://via39ii0sd.execute-api.ap-southeast-1.amazonaws.com`,
-  test: `https://l286uh2hkj.execute-api.ap-southeast-1.amazonaws.com`,
-}
+export { UserEndPoints }
--- src/pages/effectnode/index.jsx
+++ src/pages/effectnode/index.jsx
@@ -1,84 +1,12 @@
 // import importShim from
 
-import { UserEndPoints } from '@/content-landing-page/LoginContentGate/GateConst'
+// import { UserEndPoints } from '@/content-landing-page/LoginContentGate/GateConst'
+import { importPackages } from '@/engine-effectnode/lib/importPackages'
 import { useEffect } from 'react'
 //
 export default function EffectNode() {
   useEffect(() => {
     let run = async () => {
-      // import('@jspm/generator').then(({ Generator }) => {
-      //   console.log()
-      // })
-      let myEndPoint = UserEndPoints[process.env.NODE_ENV]
-      //
-      let getImportMap = async (myPackages) => {
-        return fetch(`${myEndPoint}/import-map`, {
-          method: 'POST',
-          body: JSON.stringify({
-            packages: myPackages,
-          }),
-          mode: 'cors',
-        }).then((r) => {
-          if (r.ok) {
-            return r.json()
-          } else {
-            return Promise.reject()
-          }
-        })
-      }
-
-      //
-      let installImportMapOnce = async () => {
-        let res = document.body.querySelector('#importmap')
-
-        if (!res) {
-          document.body.appendChild(
-            Object.assign(document.createElement('script'), {
-              id: 'importmap',
-              type: 'importmap-shim',
-              innerHTML: JSON.stringify({ imports: {} }),
-            })
-          )
-
-          document.body.appendChild(
-            Object.assign(document.createElement('script'), {
-              id: 'esms-options',
-              type: 'esms-options',
-              innerHTML: JSON.stringify({
-                shimMode: true,
-              }),
-            })
-          )
-
-          await import('es-module-shims')
-        }
-      }
-
-      //
-      let importPackages = async (myPackages = []) => {
-        //
-        await installImportMapOnce()
-
-        return getImportMap(myPackages)
-          .then((v) => {
-            window.importShim.addImportMap(v)
-
-            return Promise.all(
-              myPackages.map((it) => {
-                return window.importShim(it)
-              })
-            ).then((result) => {
-              //
-              // console.log(result)
-              //
-              return result
-            })
-          })
-          .catch((e) => {
-            console.warn(e)
-          })
-      }
-
       importPackages([
         'three',
         'three/examples/jsm/utils/SkeletonUtils.js',
