037e07ebea79e9d24d95a3d4c67335e661159416
Wong Lok <wonglok@users.noreply.github.com>
2023-01-03 22:37:48 +0800

    ya

---
+++ src/content-pharmecy/MetaverseContent.js
@@ -0,0 +1,122 @@
+import { usePlayAllAnim } from '@/hooks/use-play-all-anim'
+import { Collider } from '@/lib/collider/Collider'
+import { useGLBLoader } from '@/lib/glb-loader/useGLBLoader'
+// import { UIContent } from '@/lib/UIContent'
+import { WalkerGame } from '@/lib/walker/WalkerGame'
+import {
+  Box,
+  // Box,
+  Environment,
+  OrbitControls,
+  useTexture,
+  // PerspectiveCamera,
+} from '@react-three/drei'
+// import { useFrame } from '@react-three/fiber'
+// import { useXR } from '@react-three/xr'
+// import { useEffect, useMemo } from 'react'
+// import { AnimationMixer } from 'three140'
+import { Mouse3D } from '@/content-vfx/Noodle/Mouse3D'
+import { useThree } from '@react-three/fiber'
+import { useSnapshot } from 'valtio'
+import { WalkerState } from '@/lib/walker/WalkerState'
+import { AvatarChaser } from '@/content-landing-page/AvatarChaser/AvatarChaser.jsx'
+import { Noodle } from '@/content-vfx/Noodle/Noodle'
+import { CoreReady } from '@/content-landing-page/Core/Core'
+import { Avatar } from '@/content-landing-page/Avatar/Avatar'
+import { EquirectangularReflectionMapping, Group, sRGBEncoding } from 'three'
+import { useMemo } from 'react'
+import { Object3D } from 'three140'
+
+const GameName = 'Tmobile'
+
+export function MetaverseContent({
+  mapURL = `/scene/2022-11-28-NYC/NYC_Expo_30.glb`,
+}) {
+  //public
+  let glb = useGLBLoader(mapURL)
+
+  usePlayAllAnim(glb)
+
+  let texture = useTexture(`/hdr/studio_hdri_bright.png`)
+  let scene = useThree((s) => s.scene)
+  texture.mapping = EquirectangularReflectionMapping
+  texture.encoding = sRGBEncoding
+  scene.environment = texture
+
+  let camera = useThree((s) => s.camera)
+  let gl = useThree((s) => s.gl)
+
+  let walk = useSnapshot(WalkerState)
+
+  let gp = useMemo(() => {
+    let gp = new Object3D()
+    gp.add(glb.scene.getObjectByName('playerArea').clone())
+    gp.add(glb.scene.getObjectByName('Cube020').clone())
+    gp.add(glb.scene.getObjectByName('speakerScreen').clone())
+    return gp
+  }, [glb.scene])
+
+  //
+  return (
+    <group>
+      <group position={[0, 0.0, 0]}>
+        <CoreReady></CoreReady>
+        <Collider
+          scene={gp}
+          onReady={(collider) => {
+            return (
+              <group>
+                <primitive object={glb.scene}></primitive>
+
+                <OrbitControls
+                  args={[camera, gl.domElement]}
+                  makeDefault
+                ></OrbitControls>
+
+                <WalkerGame
+                  startAt={[
+                    0, 2.1, 0,
+                    // 2.563503709126706,
+                    // 1.595614002597168 + 3,
+                    // 45.14220988974003,
+                  ]}
+                  name={GameName}
+                  glb={glb}
+                  collider={collider}
+                  onGameReady={({ game, core }) => {
+                    //
+                    WalkerState[GameName] = game
+                    WalkerState.current = game
+                  }}
+                ></WalkerGame>
+
+                {/*  */}
+                {/*  */}
+
+                <AvatarChaser collider={collider}></AvatarChaser>
+
+                <group>
+                  <Mouse3D collider={collider}></Mouse3D>
+                </group>
+              </group>
+            )
+          }}
+        ></Collider>
+
+        <group>
+          <Noodle chaseName='bb00'></Noodle>
+        </group>
+      </group>
+
+      <Avatar></Avatar>
+
+      {/* <EXR url={`/hdr/exr/nebula-1k.exr`}></EXR> */}
+
+      {/* <Environment
+        // files={`/hdr/studio_hdri_bright.png`}
+        // files={`/hdr/BROADWAY_LAFAYETTE_STATION_2.hdr`}
+        // files={}
+      ></Environment> */}
+    </group>
+  )
+}
---
+++ src/content-pharmecy/Phar.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable @next/next/no-img-element */
+import { Canvas } from '@react-three/fiber'
+import { sRGBEncoding } from 'three'
+// import { Core } from '../Core/Core'
+// import { XR, Controllers, VRButton, useXR } from '@react-three/xr'
+// import { useSnapshot } from 'valtio'
+// import { GateState } from '../LoginContentGate/GateState'
+// import { SupplyXR } from '@/lib/walker/SupplyXR'
+// import { Bloom, EffectComposer, SSR } from '@react-three/postprocessing'
+import { Perf } from 'r3f-perf'
+import { MetaverseContent } from './MetaverseContent'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
+// import { Noodle } from '@/content-vfx/Noodle/Noodle'
+
+export function Phar(
+  {
+    //
+  }
+) {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        .r3f-perf {
+          display: none !important;
+        }
+        @media (min-width: 1400px) {
+          .r3f-perf {
+            display: block !important;
+          }
+        }
+      `,
+        }}
+      ></style>
+      {/*  */}
+      <Canvas
+        //
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          backgroundColor: '#F08BDC',
+        }}
+        {...{
+          gl: { antialias: true, logarithmicDepthBuffer: false },
+          onCreated: (st) => {
+            //
+
+            st.gl.physicallyCorrectLights = true
+            st.gl.outputEncoding = sRGBEncoding
+
+            st.gl.shadowMap.enabled = false
+          },
+        }}
+      >
+        {process.env.NODE_ENV === 'development' && (
+          <Perf
+            showGraph={false}
+            minimal={false}
+            className='r3f-perf'
+            style={{
+              right: 'auto',
+              top: 'auto',
+              left: 0,
+              top: 0,
+              zIndex: 2000,
+            }}
+          />
+        )}
+
+        {/*  */}
+        <MetaverseContent
+          mapURL={`/scene/2023-01-03-phar/exhibitionArea.glb`}
+          online={true}
+        ></MetaverseContent>
+
+        <EffectComposer
+          disableNormalPass
+          resolutionScale={0.1}
+          multisampling={0}
+        >
+          <Bloom
+            mipmapBlur
+            radius={0.75}
+            intensity={1.2}
+            luminanceThreshold={0.7}
+          ></Bloom>
+        </EffectComposer>
+      </Canvas>
+
+      {/* {gs.xrSession && gs.supportVR && <VRButton></VRButton>} */}
+    </>
+  )
+}
