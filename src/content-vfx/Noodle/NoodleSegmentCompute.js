487c765baf909c786ce077876fe76d148ef8cf0c
Wong Lok <wonglok@users.noreply.github.com>
2023-01-08 07:16:53 +0800

    va

--- src/content-vfx/Noodle/NoodleSegmentCompute.js
+++ src/content-vfx/Noodle/NoodleSegmentCompute.js
@@ -172,7 +172,7 @@ export class NoodleSegmentCompute {
           vec4 texColor = texture2D(headList, uvv);
 
           // // yolines
-          vec3 xyz = lerp(positionHead.rgb, texColor.rgb, 0.75);
+          vec3 xyz = lerp(positionHead.rgb, texColor.rgb, 1.0);
 
 
           gl_FragColor = vec4(xyz.rgb, 1.0);
@@ -181,15 +181,18 @@ export class NoodleSegmentCompute {
         } else {
           vec3 positionChain = texture2D( texturePosition, nextUV ).xyz;
 
-          positionChain.rgb = lerp(positionHead.rgb, positionChain.rgb, 0.5);
+          positionChain.rgb = lerp(positionHead.rgb, positionChain.rgb, 0.9);
 
           // positionChain.xyz *= 1.0 + sin(time) * 0.25 * 0.0135;
 
           positionChain.xyz = positionChain.xyz - trackerPos;
 
           // positionChain.xz *= (1.0 - gl_FragCoord.x / resolution.x * 0.03);
-          // positionChain.y += cnoise(vec3(positionChain.xyz * 1.333)) * 0.1;
-          // positionChain.xyz *= 1.9;
+          // positionChain.x += cnoise(vec3(positionChain.xyz)) * 0.1;
+          // positionChain.y += cnoise(vec3(positionChain.xyz)) * 0.1;
+          // positionChain.z += cnoise(vec3(positionChain.xyz)) * 0.1;
+
+
 
           positionChain.xyz = positionChain.xyz + trackerPos;
 
