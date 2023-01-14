821325738f08cc495286f7f86eff96ac88fe1fa6
Wong Lok <wonglok@users.noreply.github.com>
2023-01-14 13:14:10 +0800

    fafa

--- src/pages/t.jsx
+++ src/pages/t.jsx
@@ -6,6 +6,6 @@ export async function getStaticProps(context) {
   return {
     props: {
       title: 'Agape Town - Here we go!',
-    }, // will be passed to the page component as props
+    },
   }
 }
